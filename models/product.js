458fef3882861a14825e3ccefbfbf0921a46d863
k9lada00 <60172591+k9lada00@users.noreply.github.com>
2020-02-24 17:21:38 +0200

    connection to the database

---
+++ models/product.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; 
+
+const productSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    title: { type: String, required: true },
+    description: { type: String, required: false }, 
+    category: { type: String, required: false },
+    location: { type: String, required: false },
+    productImage1: { type: String, required: false },
+    productImage2: { type: String, required: false },
+    productImage3: { type: String, required: false },
+    productImage4: { type: String, required: false },
+    askingPrice: { type: Number, required: false },
+    dateOfPosting: { type: String, required: false },
+    deliveryType: { type: String, required: false },
+    sellerName: { type: String, required: false },
+    sellerContactInfo: { type: String, required: false }
+
+});
+
+module.exports = Item = mongoose.model('Product', productSchema);
